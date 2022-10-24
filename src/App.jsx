bd4c84d7c6020b2f0bc414a04aca870d4eea8bf1
minsu <cocacolazero024@gmail.com>
2022-10-24 16:18:47 +0900

    ADD Styled-Components and Reset CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Main from './Pages/Main';
+import GlobalStyles from './GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Main.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Main = () => {
+  return (
+    <>
+      <Title>Hi</Title>
+    </>
+  );
+};
+const Title = styled.h1``;
+
+export default Main;
